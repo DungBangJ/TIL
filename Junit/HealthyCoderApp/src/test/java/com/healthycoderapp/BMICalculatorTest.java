6e8d0209fcd3b67f818576217976fb4dcd770378
DungBangJ <yoyob1223@gmail.com>
2021-12-31 11:34:36 +0900

    와 어렵다. junit testing assertTrue/False/Throw/Equals/All...

--- Junit/HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ Junit/HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -3,12 +3,50 @@ package com.healthycoderapp;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class BMICalculatorTest {
 
 	@Test
 	void should_ReturnTrue_When_DietRecommended() {
-		assertTrue(BMICalculator.isDietRecommended(89.0, 1.72));
+		
+		//given: input value
+		double weight = 89.0;
+		double height = 1.72;
+		
+		//when: 메서드의 결과를 대입하는 구문
+		boolean recommended = BMICalculator.isDietRecommended(weight, height);
+		
+		//then: assertion - 실행되는지 아닌지
+		assertTrue(recommended);
+	}
+	
+	@Test
+	void should_ReturnFalse_When_DietRecommended() {
+		
+		//given: input value
+		double weight = 50.0;
+		double height = 1.92;
+		
+		//when: 메서드의 결과를 대입하는 구문
+		boolean recommended = BMICalculator.isDietRecommended(weight, height);
+		
+		//then: assertion - 실행되는지 아닌지
+		assertFalse(recommended);
+	}
+	@Test
+	void should_ThrowArithmeticException_When_DietRecommended() {
+		
+		//given: input value
+		double weight = 50.0;
+		double height = 0;
+		
+		//when: 메서드의 결과를 대입하는 구문
+		Executable executable = () -> BMICalculator.isDietRecommended(weight, height);
+		//lambda expression을 사용해서 즉시 사용할 것은 아니지만, 대신 executable을 assertThrows에 넘겨줘서
+		//이것이 예외를 던지는지만 확인한다.
+		//then: assertion - 실행되는지 아닌지
+		assertThrows(ArithmeticException.class, executable);
 	}
 
 }
--- Junit/Junit.md
+++ Junit/Junit.md
@@ -19,7 +19,28 @@ test 메소드 안에 assertTru()를 사용해서 확인하고 싶은 것을 ()�
 - test 메소드를 가독성을 높이기 위해 문장으로 메소드 이름을 표현하자.
   - should_ReturnTrue_When_DietRecommended()로 바꿔주자.
 
-unit test는 남들이 보기에 한번에 어떤 test인지 파악할 수 있을만큼 가독성이 좋게 만들어야 한다.
-- given
-- when
-- then
+unit test는 남들이 보기에 한번에 어떤 test인지 파악할 수 있을만큼 가독성이 좋게 만들어야 한다. 다음의 세개의 주석을 달아준다.
+- given: input value
+- when: 메서드의 결과를 대입하는 구문
+- then: assertion - 실행되는지 아닌지
+
+test의 결과가 참이 나오는 것을 해봤으니 거짓이 나오는 test 메소드도 만들어보자.
+- 메소드명과 안에 있는 input value 등을 모두 바꿔준다. false가 나오는 값으로 바꿔주자.(assertFalse 포함)
+
+main 폴더에 가면 exception을 던지는 것을 알 수 있다. 이를 위해서도 test를 만들어줘야 한다.
+- 이를 위한 test method의 이름은 should_ThrowArithmeticException_When_HeightZero()로 바꿔준다.
+- 또한 안에 있는 height부분을 0으로 바꿔준다.(그래야 예외가 발생한다.)
+- asset문도 assertThrows로 바꿔준다.
+  - 여기에 해당하는 파라미터는 ArithmeticException을 던져줘야 하므로 첫째 칸에는 ArithmeticException을 넣어준다.
+  - assertThrows의 두번째 파라미터를 만들어야 한다.
+    - 45번째 줄에서 보면 어차피 exception을 날리는 것을 알고 있으므로, 우리는 이 구문을 실행하는 것 대신에 구문 실행을 할 가능성만 생각해서 Executable로 바꿔준다. Ctrl+space로 junit것을 상속한다.
+
+
+@Test
+- 이 어노테이션을 쓰지 않는다면, test는 실행되지 않는다.
+
+assertTrue
+assertFalse
+assertThrows
+assertEquals
+assertAll
