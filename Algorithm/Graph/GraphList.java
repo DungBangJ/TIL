ffcc11f66e9c70755c120f3b9452cbbc819f0c75
dungbangj <yoyob1223@gmail.com>
2022-02-06 13:43:20 +0900

    Graph, GraphList

---
+++ Algorithm/Graph/GraphList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class GraphList {
+    private ArrayList<ArrayList<Integer>> listGraph;
+
+    public GraphList() {
+        listGraph = new ArrayList<ArrayList<Integer>>();
+    }
+
+    public void addDirectedEdge(int x, int y){
+        listGraph.get(x).add(y);
+    }
+
+    public void addCompleteEdge(int x, int y){
+        listGraph.get(x).add(y);
+        listGraph.get(y).add(x);
+    }
+
+    public void printGraph() {
+        for(int i=1; i<listGraph.size(); i++) {
+            System.out.print("vertex " + i + " => ");
+            for(int j=0; j<listGraph.get(i).size(); j++) {
+                System.out.print(" "+listGraph.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
+}
